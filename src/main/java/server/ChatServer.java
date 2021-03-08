2cd3abec68e525e14b1d1f3e8ba53f60c27f3857
Zero-slayer <71024460+Zero-slayer@users.noreply.github.com>
2021-03-08 14:10:36 +0100

    Added ChatServer.java

--- src/main/java/server/ChatServer.java
+++ src/main/java/server/ChatServer.java
@@ -1,15 +1,44 @@
 package server;
 
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ChatServer {
 
+    private ServerSocket serverSocket;
+    private Map<String, ClientHandler> allClientHandlers = new ConcurrentHashMap<>();
+    private BlockingQueue<String> sendQueue = new ArrayBlockingQueue<>(8);
+    private Users users = new Users();
+
+    public Map<String, ClientHandler> getAllClientHandlers() {
+        return allClientHandlers;
+    }
+
+    private void startserver(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        System.out.println("Server started, listening on " +port);
+
+        while (true) {
+            System.out.println("Waiting for a client");
+            Socket socket = serverSocket.accept(); //blocking call
+            System.out.println("New client connected");
+
+            ClientHandler clientHandler = new ClientHandler(socket, this, users);
+
+        }
+    }
 
     //Call server with arguments like this: 0.0.0.0 8088 logfile.log
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) throws IOException {
         String ip ="localhost";
         int port = 8088;
         String logFile = "log.txt";  //Do we need this
@@ -27,6 +56,9 @@ public class ChatServer {
             System.out.println("Illegal inputs provided when starting the server!");
             return;
         }
+
+        ChatServer server = new ChatServer();
+        server.startserver(port);
 
     }
 
