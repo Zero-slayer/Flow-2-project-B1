d2589953998c11ab0a2df9184adae2723a24ce06
Zero-slayer <71024460+Zero-slayer@users.noreply.github.com>
2021-03-08 14:12:40 +0100

    Added Users.java

--- src/main/java/server/Users.java
+++ src/main/java/server/Users.java
@@ -1,2 +1,33 @@
-package server;public class Users {
+package server;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Users {
+
+    private List<String> users = new ArrayList<>();
+    private List<String> onlineUsers = new ArrayList<>();
+
+    public Users() {
+        users.add("Lars");
+        users.add("Johnny");
+        users.add("Bendt");
+        users.add("Luke");
+        users.add("Bob");
+        users.add("Rey");
+    }
+
+    public List<String> getUsers() {
+        return this.users;
+    }
+
+    public void addOnlineUsers(String user) {
+        onlineUsers.add(user);
+    }
+
+    public List<String> getOnlineUsers() {
+        return onlineUsers;
+    }
+
 }
