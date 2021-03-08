bc2d4f0063e58e945fea02dfc14c22e1442a748d
Zero-slayer <71024460+Zero-slayer@users.noreply.github.com>
2021-03-08 14:11:28 +0100

    Added Message.java

--- src/main/java/server/Message.java
+++ src/main/java/server/Message.java
@@ -1,2 +1,8 @@
-package server;public interface Message {
+package server;
+
+public interface Message {
+
+    void sendMessage(String input);
+    void sendMessage(String input, String user);
+    void sendMessage(String input, String[] users);
 }
