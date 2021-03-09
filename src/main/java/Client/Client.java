6ff059061bc8c953d9d6f5177800d009e764f6b7
Alek0329 <alek0329@gmail.com>
2021-03-09 10:23:26 +0100

    Create Client
    Client created

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -1,4 +1,35 @@
 package Client;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+
+    Socket socket;
+    PrintWriter pw;
+    Scanner scanner;
+
+    public void connect(String adresse, int port) throws IOException {
+        socket = new Socket(adresse,port);
+        pw = new PrintWriter(socket.getOutputStream(),true);
+        scanner = new Scanner(socket.getInputStream());
+
+        Scanner keyBoard = new Scanner(System.in);
+        Boolean keepRunning = true;
+        while (keepRunning){
+            String msgToSend = keyBoard.nextLine();
+            pw.println(msgToSend);
+            System.out.println(scanner.nextLine());
+            if (msgToSend.equals("CLOSE#")) {
+                keepRunning = false;
+            }
+        }
+        socket.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Client().connect("localhost", 8088);
+    }
 }
